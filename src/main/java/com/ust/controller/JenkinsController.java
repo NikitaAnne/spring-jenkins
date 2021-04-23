c62c584a86dcde47ca68cb6a2c1831fcbb5fc68b
Nikita.Jacob <nikita.jacob@ust.com>
2021-04-23 07:16:41 +0530

    first commit

---
+++ src/main/java/com/ust/controller/JenkinsController.java
@@ -0,0 +1,25 @@
+package com.ust.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class JenkinsController {
+
+	@GetMapping("/")
+	public String getHomePage() {
+		return "Welcome to home page";
+	}
+	
+	@GetMapping("/admin")
+	public String getAdminPage() {
+		return "Welcome to admin page";
+	}
+	@GetMapping("/user")
+	public String getUserPage() {
+		return "Welcome to user page";
+	}
+}
+
+
